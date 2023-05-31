5d06126274623b16343e746c7cc8d797399ada9b
Kann Jittiwan <kjittiwan2@gmail.com>
2023-05-31 20:11:03 +0700

    make the about page responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         primary: '#ecf0f3',
         content: '#1f2937',
         accent: '#5651e5',
+        purple: '#f5e1fd',
+        yellow: '#ffe7a0',
+        green: '#c1e1c1',
+        pink: '#ffc0cb',
       },
       screens: {
         sm: '640px',
